c6f044f8dc89c24e6c7dbd441226ccfd36878c32
RobertMachi <robert.mc@hotmail.es>
2019-04-08 17:36:52 +0200

    Created component butoon and finished navbar styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import logo2 from '../logo2.svg';
+import styled from 'styled-components';
+import {ButtonContainer} from './Boton';
 
 export default class componentName extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expend-sm bg-primary navbar-dark px-sm">
+                <NavWrapper className="navbar navbar-expend-sm navbar-dark px-sm">
                     {/*  
                     https://www.iconfinder.com/icons/1243689/call_phone_icon
                     Creative Commons (Attribution 3.0 Unported);
@@ -22,14 +24,24 @@ export default class componentName extends Component {
                         </li>
                     </ul>
                     <Link to='/cart' className="ml-auto">
-                        <button>
-                            <i className="fas fa-cart-plus" />
+                        <ButtonContainer>
+                            <span className="mr-2">
+                                <i className="fas fa-cart-plus" />
+                            </span>
                             my cart
-                        </button>
+                        </ButtonContainer>
                     </Link>
-                </nav>
+                </NavWrapper>
             </div>
         )
     }
 }
 
+const NavWrapper = styled.nav`
+    background: var(--mainBlue);
+    .nav-link{
+        color: var(--mainWhite) !important;
+        font-size: 1.3rem;
+        text-transform: capitalize;
+    }
+`
