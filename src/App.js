fcccc4a1f5b9e2112711c870084d38836cefa097
RobertMachi <robert.mc@hotmail.es>
2019-04-08 15:51:51 +0200

    Components imported in App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
 
 class App extends Component {
   render() {
-    return (
-      <React.Fragment> 
-        <h3>
-          Hello from app
-        </h3>            
+      return (
+    <React.Fragment> 
+        <Navbar/>
+        <ProductList/>
+        <Details/>
+        <Cart/>
+        <Default/>
       </React.Fragment>
+      
     );
   }
 }
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,7 +4,7 @@ export default class Cart extends Component {
   render() {
     return (
       <div>
-        <h3>Hello from the Cart</h3>
+        <h3>Hello from the cart</h3>
       </div>
     )
   }
