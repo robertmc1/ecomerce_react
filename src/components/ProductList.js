9b6bc39cff4a25eb2acc591292bb503137faaf94
RobertMachi <robert.mc@hotmail.es>
2019-04-08 20:43:50 +0200

    deleted the state from ProductList

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,21 +1,15 @@
 import React, { Component } from 'react';
 import Product from './Product';
 import Title from './Title';
-import {storeProducts} from '../data';
 import {ProductConsumer} from '../context';
 
-
 export default class ProducList extends Component {
-    state = {
-        products: storeProducts
-    };
     render() {
         return (
             <React.Fragment>
                 <div className="py-5">
                     <div className="container">
                         <Title name="our " title="products " />
-
                         <div className="row">
                             <ProductConsumer>
                                 {(value)=>{
@@ -31,4 +25,3 @@ export default class ProducList extends Component {
         )
     }
 }
-
