0d0626202c02cf100b0f6d697a9a8a61584fe059
RobertMachi <robert.mc@hotmail.es>
2019-04-08 16:15:15 +0200

    Router working correctlly

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import {Switch, Route} from 'react-router-dom';
 //import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Navbar from './components/Navbar';
 import ProductList from './components/ProductList';
 import Details from './components/Details';
@@ -13,10 +15,12 @@ class App extends Component {
       return (
     <React.Fragment> 
         <Navbar/>
-        <ProductList/>
-        <Details/>
-        <Cart/>
-        <Default/>
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
       </React.Fragment>
       
     );
