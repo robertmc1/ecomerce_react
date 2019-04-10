0567434273e34c2af4a0c938eff63d3e78cc16ec
RobertMachi <robert.mc@hotmail.es>
2019-04-10 18:57:25 +0200

    Added: subtotal, tax & total to cart, and delete one item or clear the cart

--- src/context.js
+++ src/context.js
@@ -8,7 +8,7 @@ class ProductProvider extends Component {
     state = {
         products:[],
         detailProduct: detailProduct,
-        cart: storeProducts,
+        cart: [],
         modalOpen: false,
         modalProduct: detailProduct,
         cartSubTotal: 0,
@@ -55,7 +55,7 @@ class ProductProvider extends Component {
         this.setState(() =>{
             return {products: tempProducts, cart: [...this.state.cart, product]}
         }, () => {
-            console.log(this.state)
+            this.addTotals();
         });
     };
 
@@ -79,10 +79,49 @@ class ProductProvider extends Component {
         console.log(`this is decrement method`)
     };
     removeItem = id => {
-        console.log('item removed')
+        let tempoProducts = [...this.state.products];
+        let tempCart = [...this.state.cart];
+
+        tempCart = tempCart.filter(item => item.id !== id);
+
+        const index = tempoProducts.indexOf(this.getItem(id));
+        let removedProduct = tempoProducts[index];
+        removedProduct.inCart = false;
+        removedProduct.count = 0;
+        removedProduct.total = 0;
+
+        this.setState(() => {
+            return{
+                cart:[...tempCart],
+                products: [...tempoProducts]
+                }
+        }, () => {
+            this.addTotals();
+        })
     };
+
     clearCart = () => {
-        console.log('cart is cleared')
+        this.setState(() => {
+            return {cart:[]}
+        }, () => {
+            this.setProducts();
+            this.addTotals()
+        })
+    };
+
+    addTotals = () => {
+        let subTotal = 0;
+        this.state.cart.map(item => (subTotal += item.total))
+        const tempTax = subTotal * 0.1;
+        const tax = parseFloat(tempTax.toFixed(2));
+        const total = subTotal + tax;
+        this.setState(() => {
+            return{
+                cartSubTotal: subTotal,
+                cartTax: tax,
+                cartTotal: total
+            }
+        })
     };
 
     render() {
