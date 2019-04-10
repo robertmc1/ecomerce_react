63cdbc0e592c22a7b316d91cb454f0d023f5c043
RobertMachi <robert.mc@hotmail.es>
2019-04-10 16:10:38 +0200

    separated Cart component, Included title in cart componen, created cartColums component

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,10 +4,10 @@ export default function Title({name, title}) {
   return (
     <div className="row">
         <div className="col-10 mx-auto my-2 text-center text-tile">
-            <h1 className="text-capitalize font-weight-bold">
+            <h1 className="text-uppercase font-weight-bold">
             {name} 
                 <strong className="text-blue">
-                {title}
+                {" " + title}
                 </strong>
             </h1>
         </div>
